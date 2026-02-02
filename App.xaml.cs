1e31944abfa4cd0dad10491102a4f2c931d63a07
richt <richt@GALAXYBOOK5PRO>
2026-02-02 01:18:19 +0100

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WiiMote_To_XBox360
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
